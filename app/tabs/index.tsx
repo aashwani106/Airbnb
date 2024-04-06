fcc1ad22c491670abc2e34a55150ce6fc3b6f1d6
Ashwani Mishra <ashwani01superman@gmail.com>
2024-04-07 03:49:00 +0530

    part 1

---
+++ app/tabs/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const Page = () => {
+  return (
+    <View>
+      {/* <Text>Page</Text> */}
+      <Link href={"/modals/login"}>Login </Link>
+      <Link href={"/modals/Booking"}>Booking  </Link>
+      <Link href= {"/listing/69"}>Listing details   </Link>
+
+    </View>
+  )
+}
+
+export default Page
